64ffdee367929a23b131ca33297f2576ea403175
Yaroslav Vorobev <ssoutoro@gmail.com>
2024-11-06 20:21:07 +0500

    Создание нового проекта на селениде

--- src/test/java/ElementsTest.java
+++ src/test/java/ElementsTest.java
@@ -1,79 +1,42 @@
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import pages.TextBoxPage;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
-@DisplayName("Тесты на проверку вкладки Elements")
 public class ElementsTest {
 
-@BeforeEach
+    @BeforeEach
     void setup() {
 
-    open("https://demoqa.com/elements");
-    Configuration.timeout=10000;
+        open("https://demoqa.com/elements");
+        Configuration.timeout = 10000;
 
-}
+    }
 
     @Test
     @Tag("SMOKE")
     @DisplayName("Тест на проверку текстбокса")
     public void textBoxPositiveTest() {
 
-    String  name = "Name",
-            email = "ss@ss.com",
-            current_address = "Kukuevo",
-            permanent_address = "Belyashivo";
+        TextBoxPage textBoxPage = new TextBoxPage();
 
-    $(byText("Text Box")).click();
-    $("#userName").setValue(name);
-    $("#userEmail").setValue(email);
-    $("#currentAddress").setValue(current_address);
-    $("#permanentAddress").setValue(permanent_address);
-    $("#submit").click();
-    $("p#name").shouldHave(text(name));
-    $("p#email").shouldHave(text(email));
-    $("p#currentAddress").shouldHave(text(current_address));
-    $("p#permanentAddress").shouldHave(text(permanent_address));
-
+        textBoxPage.openTextBoxTab()
+                .setUserName("Ivan")
+                .setUserEmail("test@test.com")
+                .setCurrentAddress("Tuduevo")
+                .setPermanentAddress("Suduevo")
+                .submit()
+                .resultName("Ivan")
+                .resultEmail("test@test.com")
+                .setCurrentAddress("Tuduevo")
+                .setPermanentAddress("Suduevo");
     }
-
-    @Test
-    @Tag("SMOKE")
-    @DisplayName("Тест на проверку удаления строки из таблицы")
-    public void deletingEntryFromTable() {
-
-    $(byText("Web Tables")).click();
-    $$(".rt-tr-group").first().find("#delete-record-1").click();
-
-            for(int i=0; i<$$(".rt-td").size(); i++) {
-                $$(".rt-td").get(i).shouldNotHave(text("Vega"));
-        }
-
-    }
-
-    @Test
-    @Tags({
-            @Tag("SMOKE"),
-            @Tag("REGRESS"),
-    })
-    @DisplayName("Тест на проверку нажатия на кнопки")
-    public void clickButton() {
-
-    $(byText("Buttons")).click();
-    $(byText("Double Click Me")).doubleClick();
-    $("#doubleClickMessage").shouldBe(visible);
-    $(byText("Right Click Me")).contextClick();
-    $("#rightClickMessage").shouldBe(visible);
-    $(byText("Click Me")).click();
-    $("#dynamicClickMessage").shouldBe(visible);
-
-    }
-
-
 
 }
---
+++ src/test/java/pages/TextBoxPage.java
@@ -0,0 +1,82 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class TextBoxPage {
+
+    private SelenideElement textBoxTab = $(byText("Text Box")),
+            userNameInput = $("#userName"),
+            userEmailInput = $("#userEmail"),
+            currentAddressInput = $("#currentAddress"),
+            permanentAddressInput = $("#permanentAddress"),
+            submitBtn = $("#submit"),
+            resultName = $("p#name"),
+            resultEmail = $("p#email"),
+            resultCurrentAddress = $("p#currentAddress"),
+            resultPermanentAddress = $("p#permanentAddress");
+
+    public TextBoxPage openTextBoxTab () {
+        textBoxTab.click();
+
+        return this;
+    }
+
+    public TextBoxPage setUserName(String value) {
+        userNameInput.setValue(value);
+
+        return this;
+    }
+
+    public TextBoxPage setUserEmail(String value) {
+        userEmailInput.setValue(value);
+
+        return this;
+    }
+
+    public TextBoxPage setCurrentAddress(String value) {
+        currentAddressInput.setValue(value);
+
+        return this;
+    }
+
+    public TextBoxPage setPermanentAddress(String value) {
+        permanentAddressInput.setValue(value);
+
+        return this;
+    }
+
+    public TextBoxPage submit() {
+        submitBtn.click();
+
+        return this;
+    }
+
+    public TextBoxPage resultName(String name) {
+        resultName.shouldHave(text(name));
+
+        return this;
+    }
+
+    public TextBoxPage resultEmail(String email) {
+        resultEmail.shouldHave(text(email));
+
+        return this;
+    }
+
+    public TextBoxPage resultCurrentAddress(String currentAddress) {
+        resultCurrentAddress.shouldHave(text(currentAddress));
+
+        return this;
+    }
+
+    public TextBoxPage resultPermanentAddress(String permanentAddress) {
+        resultPermanentAddress.shouldHave(text(permanentAddress));
+
+        return this;
+    }
+
+}
